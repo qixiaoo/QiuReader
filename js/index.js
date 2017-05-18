299faed1be81c2cb93dc006f3b869b567a8cd6ca
Midgard <248969463@qq.com>
2017-05-18 22:07:01 +0800

    ...

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-var bookStr = '';
+var bookStr = '<div class="book-head {background}"><div class="cover"><div class="cover-border"><div class="book-title">{book-name}</div></div></div><div class="mask-content"><div class="mak-top clear"><i class="icon-delete right" title="删除书籍"></i><i class="icon-edit right" title="编辑书籍信息"></i></div><div class="mask-bottom"><button class="open-book btn-flat">OPEN BOOK</button></div></div><div class="mask"></div></div><div class="book-body"><div class="author-box"><span>作者：</span><span>{author}</span></div></div><div class="book-control"></div>';
 
-var book = null;
-
+// 添加书籍按钮的事件处理程序
 document.getElementById('add-book')
     .addEventListener('change', function (e) {
 
@@ -12,7 +11,7 @@ document.getElementById('add-book')
             reader.readAsArrayBuffer(firstFile);
 
             reader.onload = function (e) {
-                book = ePub({
+                var book = ePub({
                     bookPath: e.target.result
                 });
 
@@ -20,9 +19,9 @@ document.getElementById('add-book')
                     var key = metadata.identifier,
                         name = metadata.bookTitle,
                         author = metadata.creator,
-                        // description = metadata.description,
                         article = new Book(key, name, author, e.target.result);
 
+                    console.log(metadata);
                     addBookToPage(article);
                     bookDB.open(function () {
                         bookDB.addBook(
@@ -47,19 +46,89 @@ document.getElementById('add-book')
         }
     });
 
+// 编辑书籍信息的事件处理程序（事件委托）
+document.getElementsByClassName('book-list')[0]
+    .addEventListener('click', function (e) {
+        var target = e.target;
+        if (target && target.nodeName.toLocaleLowerCase() === 'i' && target.className.indexOf('edit') !== -1) {
+            var book = target
+                .parentNode
+                .parentNode
+                .parentNode
+                .parentNode;
+            // TODO 处理程序
+        }
+    });
+
+// 删除书籍的事件处理程序（事件委托）
+document.getElementsByClassName('book-list')[0]
+    .addEventListener('click', function (e) {
+        var target = e.target;
+        if (target && target.nodeName.toLocaleLowerCase() === 'i' && target.className.indexOf('delete') !== -1) {
+            var book = target
+                .parentNode
+                .parentNode
+                .parentNode
+                .parentNode;
+            this.removeChild(book);
+        }
+    });
+
+// 打开书籍按钮的事件处理程序（事件委托）
+document.getElementsByClassName('book-list')[0]
+    .addEventListener('click', function (e) {
+        var target = e.target;
+        if (target && target.nodeName.toLocaleLowerCase() === 'i' && target.className.indexOf('open-book') !== -1) {
+            var book = target
+                .parentNode
+                .parentNode
+                .parentNode
+                .parentNode;
+            // TODO 处理程序
+        }
+    });
+
+// 打开页面时初始化书籍列表
+function init() {
+    'use strict';
+
+    bookDB.open(function () {
+        bookDB.getBooks(
+            function (books) {
+                console.log(books);
+                books.forEach(function (book) {
+                    addBookToPage(book)
+                });
+            },
+            function () {
+                alert('获取书籍信息失败，请尝试刷新页面');
+            }
+        );
+    });
+}
+
+// 向页面添加一本书籍
 function addBookToPage(obj) {
     'use strict';
 
     var list = document.getElementsByClassName('book-list')[0],
-        book = document.createElement('div');
+        book = document.createElement('div'),
+        bg,
+        str;
+
+    bg = 'bg-' + nextBG();
 
     book.className = 'book';
     book.setAttribute('data-key', obj.key);
     // 添加书籍名，其他信息
-    book.innerHTML = bookStr;
+    str = bookStr.replace('{book-name}', obj.name)
+        .replace('{author}', obj.author)
+        .replace('{background}', bg);
+    book.innerHTML = str;
     list.appendChild(book);
 }
 
+// 定义书籍的构造器
 function Book(key, name, author, content) {
     'use strict';
 
@@ -68,3 +137,17 @@ function Book(key, name, author, content) {
     this.author = author;
     this.content = content;
 }
+
+// 获取下一张背景图序号
+var nextBG = (function () {
+    var number = 0;
+
+    return function () {
+        number += 1;
+        return number % 5 + 1;
+    }
+}());
+
+window.onload = function () {
+    init();
+};
